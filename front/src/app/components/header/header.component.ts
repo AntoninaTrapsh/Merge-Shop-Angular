253f5c9fad787a0c8255c948234ed729845324f8
AntoninaTrapsh <antrapsh@gmail.com>
2021-11-29 19:48:52 +0400

    4.4. Angular. RxJs, Observable (optional)

--- front/src/app/components/header/header.component.ts
+++ front/src/app/components/header/header.component.ts
@@ -1,22 +1,28 @@
-import {Component, OnInit, Output, EventEmitter} from '@angular/core';
+import {Component, OnInit, Output, EventEmitter, OnDestroy} from '@angular/core';
 import {AuthService} from "../../auth.service";
 import {CartService} from "../../services/cart.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
   quantityProduct:number = 0;
+  private cart: Subscription = new Subscription();
+
   @Output() createCart = new EventEmitter<void>();
 
   constructor(public auth: AuthService, private cartService: CartService) { }
 
   ngOnInit(): void {
-    this.cartService.getCountProduct().subscribe((data) => {
+    this.cart = this.cartService.getCountProduct().subscribe((data) => {
       this.quantityProduct = data;
     })
   }
 
+  ngOnDestroy():void {
+    this.cart.unsubscribe();
+  }
 }
